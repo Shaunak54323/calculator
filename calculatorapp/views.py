e725a2fabf52ee04ea0a5880b6a80b4b1fc6ddb2
Shaunak <bagadeshaunak@gmail.com>
2021-01-03 10:57:50 +0530

    Calculator

---
+++ calculatorapp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, "index.html")
+
+def submitquery(request):
+    q = request.GET['query']
+    try:
+        ans = eval(q)
+        mydictionary = {
+            "q":q,
+            "ans":ans,
+            "error":False,
+        }
+        return render(request, 'index.html', context=mydictionary)
+    except:
+        mydictionary={
+            'error':True
+        }
+        return render(request, 'index.html', context=mydictionary)
