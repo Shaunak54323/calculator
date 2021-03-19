c8f70a33c184d1f122610ba7f1fe34a3ff08fc4e
Shaunak <bagadeshaunak@gmail.com>
2021-03-19 19:05:10 +0530

    Commit

--- Calculator/urls.py
+++ Calculator/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('', include('rootapp.urls')),
     path('admin/', admin.site.urls),
-    path('calculatorapp',include('calculatorapp.urls')),
+    path('calculatorapp', include('calculatorapp.urls')),
 ]
