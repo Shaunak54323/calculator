cf81c4c4ac18d39cc605031c152217bc6809ceba
Shaunak <72689713+Shaunak54323@users.noreply.github.com>
2021-03-19 19:05:52 +0530

    Update urls.py

--- Calculator/urls.py
+++ Calculator/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('', include('rootapp.urls')),
     path('admin/', admin.site.urls),
-    path('calculatorapp',include('calculatorapp.urls')),
+    path('calculatorapp', include('calculatorapp.urls')),
 ]
