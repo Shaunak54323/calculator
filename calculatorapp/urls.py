6bf44b853f21d7e6ae65e3e84c7533f06363ecb2
Shaunak <bagadeshaunak@gmail.com>
2021-03-19 19:08:26 +0530

    Commit

--- calculatorapp/urls.py
+++ calculatorapp/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('',views.index, name="index"),
+    path('', views.index, name="index"),
     path('submitquery', views.submitquery, name="submitquery")
 ]
