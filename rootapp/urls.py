619868f7f9aff0a7809cf54b9a490d728abdc06a
Shaunak54323 <bagadeshaunak@gmail.com>
2021-04-19 10:11:57 +0530

    secondary commit

--- rootapp/urls.py
+++ rootapp/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.root,name="root"),
+    path('', views.root, name="root"),
 ]
